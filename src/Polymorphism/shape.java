5b72ba3bf1ea379c783d037ffe6a8d0d556d6e69
Aryan7905 <aryansrivastava354@gmail.com>
2025-08-27 04:01:13 +0530

    Exception Handling

--- src/Polymorphism/shape.java
+++ src/Polymorphism/shape.java
@@ -1,10 +1,12 @@
 public class shape {
+
     public void draw(double l){
-        double area = l*l;
-        System.out.println("Area of square  "+area);
+        double area = l * l;
+        System.out.println("Area of square  " + area);
     }
+
     public void draw(double l, double b){
-        double area = l*b;
-        System.out.println("Area of rectangle  "+area);
+        double area = l * b;
+        System.out.println("Area of rectangle  " + area);
     }
 }
