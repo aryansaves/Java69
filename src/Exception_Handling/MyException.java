818b8c003275fc67fd027b554e2d7434fff73561
Aryan7905 <aryansrivastava354@gmail.com>
2025-08-28 04:14:18 +0530

    Exception Handling

--- src/Exception_Handling/MyException.java
+++ src/Exception_Handling/MyException.java
@@ -4,7 +4,7 @@ public class MyException {
     public static void main(String[] args) {
         UserDefinedException u = new UserDefinedException();
         try {
-            u.validate(12);
+//            u.validate(12);
             u.validate(19);
         } catch(InvalidAgeException e){
             System.out.println(e.getMessage());
