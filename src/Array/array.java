c6ebd251502008cbb1ea64a6f62cae04212a2d21
Aryan7905 <aryansrivastava354@gmail.com>
2025-09-01 15:05:12 +0530

    Arrays

---
+++ src/Array/array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Element at index " +i+ " is " + a[i]);
+        }
+    }
+}
