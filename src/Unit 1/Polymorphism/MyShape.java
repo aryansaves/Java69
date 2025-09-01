5800f6749fef7e89ec97128397d64a8fa18ce65e
Aryan7905 <aryansrivastava354@gmail.com>
2025-09-01 15:49:33 +0530

    Arrays

--- src/Unit 1/Polymorphism/MyShape.java
+++
@@ -1,7 +0,0 @@
-public class MyShape {
-    public static void main(String[] args) {
-        shape s = new NewShape();
-        s.draw(10);
-        s.draw(10,13);
-    }
-}
--- src/Unit 1/Polymorphism/NewShape.java
+++
@@ -1,6 +0,0 @@
-public class NewShape extends shape{
-    public void draw(double l){
-        double area = l*l;
-        System.out.println("Area of Square "+area);
-    }
-}
